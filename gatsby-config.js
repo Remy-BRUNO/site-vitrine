95ad4ba26fb6a4ce08ca8e5a71fa7525f70aad02
Reiggnar <bruno.remy.sebastien@gmail.com>
2024-02-04 10:49:32 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `site-vitrine-2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
